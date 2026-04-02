3c8d66531b0141b7f182a55d83c54d2350b6b950
hawraawaleed33-collab <hawraawaleed33@gmail.com>
2026-04-02 13:08:19 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # ===============================
 # MongoDB Connection
 # ===============================
-MONGO_URI = "mongodb+srv://user:user246810@cluster0.jeyjiot.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://user:user246810@cluster0.jeyjiot.mongodb.net/developers_db?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
 db = client["developers_db"]
 
